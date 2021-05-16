a3820c2a21b42abca5e91f95e3bcf8f4a7b0a03a
s012211 <62129436+s012211@users.noreply.github.com>
2021-05-16 14:51:55 +0300

    2
    Ошибка: float и double неверные. Нужно вычисления выражения расписать на несколько переменных, а не в одну переменную(для float и для double)

--- Lab-1/Lab-1/Lab-1/Lab-1.cpp
+++ Lab-1/Lab-1/Lab-1/Lab-1.cpp
@@ -9,11 +9,19 @@ int main()
 {
     setlocale(LC_CTYPE, "Russian");
 
-    int a = 100;
-    float b = 0.001;
+    float aF = 100;
+    float bF = 0.001;
+    
+    double aD = 100;
+    double bD = 0.001;
 
-    float res1 = pow((a - b), 4) - (pow(a, 4) - 4 * pow(a, 4) * b) / 6*pow(a,2)*pow(b,2) - 4*a*pow(b, 3) + pow(b, 4);
-    double res2 = pow((a - b), 4) - (pow(a, 4) - 4 * pow(a, 4) * b) / 6 * pow(a, 2) * pow(b, 2) - 4 * a * pow(b, 3) + pow(b, 4);
+    float res1_1 = pow((aF - bF), 4) - (pow(aF, 4) - 4 * pow(aF, 4) * bF);
+    float res1_2 = 6*pow(aF,2)*pow(bF,2) - 4*aF*pow(bF, 3) + pow(bF, 4);
+    float res1 = res1_1 / res1_2;
+    
+    double res2_1 = pow((aD - bD), 4) - (pow(aD, 4) - 4 * pow(aF, 4) * bD);
+    double res2_2 = 6*pow(aD,2)*pow(bD,2) - 4*aD*pow(bD, 3) + pow(bD, 4);
+    double res2 = res2_1 / res2_2;
     
     cout << "Результат выполнения(float): "<< res1 <<" \n";
     cout << "Результат выполнения(double): " << res2 << " \n";
