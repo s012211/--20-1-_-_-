da9e95c80976eed5785bb64a2ecc704cbfd2a8fb
s012211 <62129436+s012211@users.noreply.github.com>
2021-05-31 11:03:52 +0300

    13
    13

---
+++ Lab-11/Lab-11/Lab-11.cpp
@@ -0,0 +1,257 @@
+﻿// Лабораторная работа №11
+// Вариант 1
+
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE
+#include "windows.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <locale>
+#include <iostream>;
+
+using namespace std;
+
+FILE* file;										// Переменная файла
+const char* filename = "output.txt";
+
+/* Структура однонаправленного списка */
+struct point {
+	char *key;
+	point* next;		// Указатель на следующий элемент
+};
+
+/* Создание списка. Возвращает указатель на голову списка */
+point* create_list() {
+	int n;
+	char in[55];
+
+	cout << "Введите количество элементов списка: ";
+	cin >> n;
+
+
+	cout << "Введите Ключ: ";
+	cin >> in;
+
+	point* head, * p;				// Голова нового списка head и временный элемент р
+	if (n == 0)						// Создание пустого списка при количестве элементов равном 0
+		head = NULL;
+	else {
+		head = new(point);			// Создание головного элемента списка
+		head->key = in;	
+		head->next = NULL;			// Поскольку это последний элемент в списке, то "хвоста" у него нет
+
+		// В цикле происходит аналогичное создание оставшихся n-1 элементов, но при этом элемент p опережает голову head,
+		// чтобы оставалась возможность сохранить её в хвост p. В конце итерации head присваивается значение p и он
+		// снова становится головным элементом
+		for (int i = n - 1; i > 0; i--) {	
+			p = new(point);
+			
+			cout << "Введите Ключ: ";
+			cin >> in;
+			p->key = in;
+			
+			p->next = head;     
+			head = p;           
+		}
+	}
+	return head;
+}
+
+/* Добавление элемента в список */
+point* add_to_list(point* head) {
+	int el, num;
+	char in[55];
+
+	cout << "Введите элемент для добавления: ";
+	cin >> el;
+	cout << "Введите Ключ: ";
+	cin >> in;
+	cout << "Введите номер элемента для добавления: ";
+	cin >> num;
+
+	point* p, * temp;				// Вспомогательные указатели для сохранения головы исходного списка и вставки элемента
+	p = head;						// Сохранение исходной головы
+	int count = 2;
+
+	// Если вставляется в первую позицию, то элемент становится головным и следующим имеет исходную голову
+	if (num == 1) {
+		temp = new(point);
+		temp->key = in;
+		temp->next = head;
+		head = temp;			// Новый указатель на голову - добавленный элемент
+	} else {
+		// Обход списка до конца
+		while (head != NULL) {
+			// Если номер текущего элемента совпадает с номером вставляемого, то вставляется элемент с заданным ключом
+			if (count == num) {
+				temp = new(point);
+				temp->key = in;
+				temp->next = head->next;
+				head->next = temp;
+				break;						// Преждевременный выход из цикла
+			}
+			count++;
+			head = head->next;		// Продвижение по списку
+		}
+		head = p;				// Возвращение указателя на голову
+	}
+	return head;
+}
+
+/* Удаление элемента из списка */
+point* delete_from_list(point* head) {
+	int num;
+	cout << "Введите Ключ элемента для удаления: ";
+	cin >> num;
+
+	point* p, * q;
+	bool find_el = false;		// Флаг состояния поиска (true - элемент найден, false - иначе)
+	
+	p = head;					// Сохранение указателя на голову исходного списка
+
+	// Если требуется удалить первый элемент, то голове присваевается хвост
+	if (num == 1) 
+		head = p->next;
+	else {
+		int count = 2;
+		// Цикл остановится либо в конце списка, либо когда следующий элемент искомый
+		while (!find_el && head != NULL) {
+			if (count == num) find_el = true;
+			else {
+				head = head->next;
+				count++;
+			}
+		}
+		if (!find_el) {
+			cout << "Элемент не найден." << endl;
+		} else {
+			q = head->next;				// Установка указателя q на удаляемый элемент
+			head->next = q->next;		// Пропуск удаляемого элемента
+			delete(q);
+		}
+		head = p;				// Возвращение указателя на голову
+	}
+
+	return head;
+}
+
+/* Печать списка */
+void print_list(point* head) {
+	point* p;
+	p = head;
+
+	if (head == NULL)						// Вывод сообщения о пустом списке
+		cout << "Список пустой.";
+
+	// Печать списка до конца
+	while (head != NULL) {
+		cout << "Ключ: " << head->key << "\n";
+		head = head->next;
+	}
+	
+	cout << endl;
+	head = p;
+}
+
+/* Запись списка в файл */
+void write_list_in_file(point* head) {
+	file = fopen(filename, "w");		// Открытие файла на запись (комментарии в print_list)
+
+	point* p;
+	p = head;
+
+	while (head != NULL) {
+		fprintf(file, "%d ", head->key);
+		head = head->next;
+	}
+	cout << "Список записан в файл." << endl;
+	head = p;
+
+	fclose(file);
+}
+
+/* Уничтожение списка */
+point* delete_list(point* head) {
+	point* q;
+	while (head != NULL) {
+		q = head;				// Сохранение указателя на текущий элемент списка
+		head = head->next;		// Перемещение указателя по списку далее
+		delete(q);				// Удаление сохраненного указателя
+	}
+	head = NULL;	// Указатель никуда не указывает
+	cout << "Список успешно удалён.\n";
+	return head;
+}
+
+/* Восстановление списка из файла */
+point* read_list_from_file() {
+	file = fopen(filename, "r");		// Открытие файла на чтение
+	point* head, * p, * q;
+
+	// Если сразу обнаружен конец файла, значит список пуст, иначе создается список и считывается из файла
+	if (feof(file))
+		head = NULL;  
+	else {
+		char n[55];							// Создание аналогично функции create_list
+		head = new(point);
+		fscanf(file, "%d", &n);
+		head->key = n;
+		head->next = NULL;
+		q = head;
+		fscanf(file, "%d", &n); 
+
+		while (!feof(file)) {
+			p = new(point);
+			p->key = n;
+			p->next = NULL;
+			head->next = p;
+			head = head->next;
+			fscanf(file, "%d", &n);
+		}
+		head = q;
+	}
+	fclose(file);
+	return head;
+}
+
+void main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "russian");	
+	system("chcp 1251");
+
+	point* head;
+
+	cout << endl << "* Создание списка *" << endl;
+	head = create_list(); 
+
+	cout << endl << "* Печать списка *" << endl;
+	print_list(head);       
+
+	cout << endl << "* Добавление элемента в список *" << endl;
+	head = add_to_list(head);
+	cout << "Результат:" << endl;
+	print_list(head);
+
+	cout << endl << "* Удаление элемента из списка *" << endl;
+	head = delete_from_list(head);
+	cout << "Результат:" << endl;
+	print_list(head);
+
+	cout << endl << "* Запись списка в файл *" << endl;
+	write_list_in_file(head); 
+
+	cout << endl << "* Уничтожение списка *" << endl;
+	head = delete_list(head);
+
+	cout << endl << "* Печать списка *" << endl;
+	print_list(head);
+
+	cout << endl << "* Восстановление списка из файла *" << endl;
+	//head = read_list_from_file();
+	//print_list(head);  
+
+	cout << endl << "* Уничтожение списка *" << endl;
+	//delete_list(head);
+}
---
+++ Lab-8/Lab-8/Lab-8.cpp
@@ -0,0 +1,199 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE 
+#include "windows.h"
+#include <locale>
+#include <iostream>
+
+using namespace std;
+
+FILE* file;	
+int n;		
+
+
+struct OwnerAvto {
+	char lastname[50];			
+	char name[50];				
+	char patronymic[50];		
+	int num_avto;
+	int pasport;
+	int gai;
+};
+
+OwnerAvto create() {
+	OwnerAvto el;
+	cout << "Фамилия: ";
+	cin >> el.lastname;
+
+	cout << "Имя: ";
+	cin >> el.name;
+
+	cout << "Отчество: ";
+	cin >> el.patronymic;
+
+	cout << "Номер авто: ";
+	cin >> el.num_avto;
+
+	cout << "Номер техпаспорта: ";
+	cin >> el.pasport;
+
+	cout << "Отделение регистрации ГАИ: ";
+	cin >> el.gai;
+
+	return el;
+}
+
+void createFile() {
+	cout << "*Формирование файла*" << endl;
+
+	file = fopen("OwnerAvto.dat", "wb");
+
+	if (file == NULL) {
+		cout << "Ошибка при открытии файла";
+		exit(1);
+	}
+
+	cout << "Для начала формирования файла введите первый элемент" << endl;
+	OwnerAvto el = create();
+	fwrite(&el, sizeof(OwnerAvto), 1, file);
+
+	if (ferror(file) != NULL) {
+		cout << "Ошибка при записи в файл";
+		exit(2);
+	}
+
+	n = 1;
+	cout << "*Файл сформирован*" << endl;
+	fclose(file);
+}
+
+void readFile() {
+	cout << "--- Содержимое файла ----" <<  endl;
+	file = fopen("OwnerAvto.dat", "rb");
+
+	OwnerAvto el;
+	int k = 0;
+	while (!feof(file) && k < n) {
+		fread(&el, sizeof(OwnerAvto), 1, file);
+		
+
+		cout << "Фамилия: " << el.lastname << endl;
+		cout << "Имя: " << el.name << endl;
+		cout << "Отчество: " << el.patronymic << endl;
+		cout << "Номер авто: " << el.num_avto << endl;
+		cout << "Номер техпаспорта: " << el.pasport << endl;
+		cout << "Отделение регистрации ГАИ: " << el.gai << endl;
+
+		cout << "---------" << endl;
+		k++;
+	}
+	fclose(file);
+}
+
+void addFile() {
+	cout << "*Добавление нового элемента в файл" << endl;
+	OwnerAvto new_el;
+	new_el = create();
+
+	char after_sur[50];										
+	cout << endl << "После элемента с какой фамилией добавить новый элемент?" << endl;
+	cin >> after_sur;
+
+	file = fopen("OwnerAvto.dat", "rb");
+
+
+	FILE* temp;
+	temp = fopen("temp.dat", "wb");
+
+
+	bool isAdded = false;
+	OwnerAvto el;
+	for (int i = 1; i <= n; i++) {
+		fread(&el, sizeof(OwnerAvto), 1, file);
+
+
+		fwrite(&el, sizeof(OwnerAvto), 1, temp);
+
+
+		if (!strcmp(el.lastname, after_sur)) {
+			fwrite(&new_el, sizeof(OwnerAvto), 1, temp);
+
+			if (ferror(temp) != NULL) {
+				exit(2);
+			}
+
+			isAdded = true;
+		}
+	}
+
+	if (isAdded) {
+		n++;
+		cout << "Элемент добавлен" << endl;
+	}
+	else cout << endl << "Элемента с выбранной фамилией не найдено" << endl;
+
+	fclose(file);
+	fclose(temp);
+	remove("OwnerAvto.dat");
+	rename("temp.dat", "OwnerAvto.dat");
+}
+
+void deleteFile() {
+	int num;
+	cout << "*Удаление элемента из файла*" << endl;
+	cout << "Укажите номер элемента для удаления: ";
+	cin >> num;
+
+	file = fopen("OwnerAvto.dat", "rb");
+
+	FILE* temp;
+	temp = fopen("temp.dat", "wb");
+
+	OwnerAvto el;
+	for (int i = 1; i <= n; i++) {
+		fread(&el, sizeof(OwnerAvto), 1, file);
+
+		if (ferror(file) != NULL) {
+			exit(4);
+		}
+
+		if (i != num) {
+			fwrite(&el, sizeof(OwnerAvto), 1, temp);
+
+			if (ferror(temp) != NULL) {
+				exit(2);
+			}
+		}
+		else {
+			n--;
+			break;
+		}
+	}
+	fclose(file);
+	fclose(temp);
+	remove("OwnerAvto.dat");
+	rename("temp.dat", "OwnerAvto.dat");
+}
+
+void main() {
+	setlocale(LC_ALL, "russian");
+
+	SetConsoleCP(1251);	
+	SetConsoleOutputCP(1251);
+
+	createFile();
+
+	int decision = -1;
+	while (decision != 0) {
+		switch (decision) {
+		case 1: readFile(); break;
+		case 2: addFile(); break;
+		case 3: deleteFile(); break;
+		}
+		if (n == 0) exit(1);
+		cout << endl << "~ Выберите дальнейшее действие: 1 - читать файл, 2 - добавить в файл, 3 - удалить из файла ~" << endl << "~ ";
+		cin >> decision;
+		cout << endl;
+	}
+
+	system("pause");
+}
